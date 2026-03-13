91930e27e6a6240d2ec7c78aeadcab2a83241d6b
IlayBor <ilaybor2004@gmail.com>
2026-03-13 23:19:16 +0200

    fix: correct Jinja syntax in int_games_to_fetch.sql for relation check

--- dbt_project/models/intermediate/Game/int_games_to_fetch.sql
+++ dbt_project/models/intermediate/Game/int_games_to_fetch.sql
@@ -4,10 +4,10 @@
 {%- set relation = adapter.get_relation(
     database="iceberg",
     schema="staging",
-    identifier="stg_steam_api__games_details") -%}
+    identifier="stg_steam_api__games_details") %}
 
 select distinct cheapshark_deals.steam_app_id from {{ ref("stg_cheapshark_api__game_deals") }} cheapshark_deals
-{%- if relation is not none -%}
+{%- if relation is not none %}
 except
 select distinct steam_app_id from {{ ref("stg_steam_api__games_details") }}
 {% endif %}
