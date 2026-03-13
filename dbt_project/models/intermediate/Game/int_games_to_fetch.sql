54be6aff69f2e93877fc6706eff16bf47ddc0186
IlayBor <ilaybor2004@gmail.com>
2026-03-13 23:05:53 +0200

    fix: add dependency reference for stg_steam_api__games_details in int_games_to_fetch.sql

--- dbt_project/models/intermediate/Game/int_games_to_fetch.sql
+++ dbt_project/models/intermediate/Game/int_games_to_fetch.sql
@@ -1,4 +1,5 @@
 {{ config(materialized='table') }}
+-- depends_on: {{ ref('stg_steam_api__games_details') }}
 
 {%- set relation = adapter.get_relation(
     database="iceberg",
@@ -6,9 +7,7 @@
     identifier="stg_steam_api__games_details") -%}
 
 select distinct cheapshark_deals.steam_app_id from {{ ref("stg_cheapshark_api__game_deals") }} cheapshark_deals
-
 {%- if relation is not none -%}
 except
-select distinct steam_app_id
-from {{ ref("stg_steam_api__games_details") }}
+select distinct steam_app_id from {{ ref("stg_steam_api__games_details") }}
 {% endif %}
