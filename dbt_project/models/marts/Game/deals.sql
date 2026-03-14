4208756ee2328b29b34f3df2b6213bfe151dba65
IlayBor <ilaybor2004@gmail.com>
2026-03-14 16:11:50 +0200

    feat: add deals model to streamline game deal data retrieval and remove deprecated deals_games model

---
+++ dbt_project/models/marts/Game/deals.sql
@@ -0,0 +1,24 @@
+select 
+	cheapshark."deal_id", 
+	cheapshark."deal_rating",
+	steam_details."name",
+	cheapshark."steam_rating_percent",
+	cheapshark."steam_rating_count",
+	cheapshark."normal_price",
+	cheapshark."sale_price",
+	cheapshark."discount_percent",
+	steam_details."steam_app_id", 
+	steam_details."short_description",
+	steam_details."header_image",
+	steam_details."website",
+	steam_details."required_age",
+	steam_details."required_age",
+	steam_details."total_recommendations",
+	steam_details."metacritic_score",
+	steam_details."is_support_controller",
+	steam_details."is_available_on_windows",
+	steam_details."is_available_on_mac",
+	steam_details."is_available_on_linux",
+	steam_details."release_date"
+from {{ ref("stg_cheapshark_api__game_deals") }} cheapshark 
+inner join {{ ref("stg_steam_api__games_details") }} steam_details on (cheapshark."steam_app_id" = steam_details."steam_app_id")
